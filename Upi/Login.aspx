9ebc83ad4bfcd1f7c64089f68bada5c290840626
Marco Rodriguez Navarro <marcorodnav@gmail.com>
2018-03-07 20:19:47 -0600

    agregado login falso

---
+++ Upi/Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Upi.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        INGRESO DE USUARIOS<br />
+        <br />
+        USUARIO:
+        <asp:TextBox ID="idTextUsuario" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        CLAVE:
+        <asp:TextBox ID="idTextClave" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="idLabelUsuarioExiste" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="idBotonLogin" runat="server" OnClick="idBotonLogin_Click" Text="Login" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Upi/Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Upi
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void idBotonLogin_Click(object sender, EventArgs e)
+        {
+            string connectionStringEmpleados = System.Configuration.ConfigurationManager.ConnectionStrings["ConnStringDb1"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(connectionStringEmpleados);
+            conexion.Open();
+            SqlCommand comando = new SqlCommand("select * from usuario where usaurio ='"+idTextUsuario.Text+"' and clave='"+idTextClave.Text+"'", conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+                idLabelUsuarioExiste.Text = "El usuario " + registro["usaurio"] + " si existe";
+            } else
+            {
+                idLabelUsuarioExiste.Text = "El usuario " + idTextUsuario.Text + " no existe";
+            }
+            conexion.Close();
+            
+        }
+    }
+}
